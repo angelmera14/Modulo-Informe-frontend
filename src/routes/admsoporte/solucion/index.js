7210cb201529faba1921c5769c8818033d1a33ae
DEBBIE ENGRACIA <76543278+debbiejannith@users.noreply.github.com>
2023-02-27 18:36:31 -0500

    Update index.js

--- src/routes/admsoporte/solucion/index.js
+++ src/routes/admsoporte/solucion/index.js
@@ -1,6 +1,6 @@
 import { lazy } from 'react';
 import { Loadable } from '../../../utils/loadable';
-// ***************** DESARROLLADOR => ALLAN HERRERA *********************
+
 // ========================== INICIO ===================================
 const Solucion = Loadable(lazy(() => import('../../../sections/admsoporte/solucion/solucion')));
 
