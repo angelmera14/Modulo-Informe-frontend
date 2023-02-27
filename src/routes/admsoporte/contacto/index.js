1ac9e890e937579cdf7e4ad707f06995c69c6319
DEBBIE ENGRACIA <76543278+debbiejannith@users.noreply.github.com>
2023-02-27 18:29:31 -0500

    Update index.js

--- src/routes/admsoporte/contacto/index.js
+++ src/routes/admsoporte/contacto/index.js
@@ -1,6 +1,6 @@
 import { lazy } from 'react';
 import { Loadable } from '../../../utils/loadable';
-// ***************** DESARROLLADOR => ALLAN HERRERA *********************
+
 // ========================== INICIO ===================================
 const Contacto = Loadable(lazy(() => import('../../../sections/admsoporte/contacto/contacto')));
 
