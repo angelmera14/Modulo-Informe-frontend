7f0c2b458d2b13f5ddb6e7b35e2650040e4d3398
DEBBIE ENGRACIA <76543278+debbiejannith@users.noreply.github.com>
2023-02-27 18:26:36 -0500

    Update index.js

--- src/routes/admsoporte/asignacion/index.js
+++ src/routes/admsoporte/asignacion/index.js
@@ -1,6 +1,6 @@
 import { lazy } from 'react';
 import { Loadable } from '../../../utils/loadable';
-// ***************** DESARROLLADOR => ALLAN HERRERA *********************
+
 // ========================== INICIO ===================================
 const Asignacion = Loadable(lazy(() => import('../../../sections/admsoporte/asignacion/asignacion')));
 
