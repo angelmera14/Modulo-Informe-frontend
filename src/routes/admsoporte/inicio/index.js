43e9ad1225bb98050dbc12dcd694490cbd42a116
DEBBIE ENGRACIA <76543278+debbiejannith@users.noreply.github.com>
2023-02-27 18:31:43 -0500

    Update index.js

--- src/routes/admsoporte/inicio/index.js
+++ src/routes/admsoporte/inicio/index.js
@@ -1,6 +1,6 @@
 import { lazy } from 'react';
 import { Loadable } from '../../../utils/loadable';
-// ***************** DESARROLLADOR => ALLAN HERRERA *********************
+
 // ========================== INICIO ===================================
 const Inicio = Loadable(lazy(() => import('../../../sections/admsoporte/inicio/inicio')));
 
